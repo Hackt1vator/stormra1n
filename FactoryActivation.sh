b98134a280fddb79f1609eac7015b810c2f9a75d
Hacktivator <118433308+Hackt1vator@users.noreply.github.com>
2023-04-11 00:54:11 +0200

    Add files via upload

--- FactoryActivation.sh
+++ FactoryActivation.sh
@@ -1,28 +1,3 @@
-
-# iOS 12-14 Signal iCloud Bypass
-
-if ! which curl >> /dev/null; then
-    echo "Error: curl not found"
-    exit 1
-fi
-if ! which iproxy >> /dev/null; then
-    echo "Error: iproxy not found"
-    exit 1
-fi
-
-rm -rf ~/.ssh/known_hosts
-clear
-
-# Change the current working directory
-cd "`dirname "$0"`"
-
-
-echo 'Starting iproxy...'
-
-killall iproxy
-idevicepair pair
-#iproxy 2222:22 > /dev/null 2>&1 &
-iproxy 2222 22 &>/dev/null &
 echo ""
 echo "
 
@@ -33,22 +8,37 @@ echo "
 "
 echo ""
 
-sleep 2
-    
-echo 'Mounting filesystem as read-write'
-sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@localhost -p2222 'mount_filesystems'
-
-echo 'Running iCloud Bypass...'
-sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@localhost -p2222 'cp -f /System/Library/PrivateFrameworks/MobileActivation.framework/Support/Certificates/FactoryActivation.pem /System/Library/PrivateFrameworks/MobileActivation.framework/Support/Certificates/RaptorActivation.pem'
 
 
-sleep 2
+rm -rf ~/.ssh/known_hosts
+
+
+
+# Change the current working directory
+cd "`dirname "$0"`"
+
+sleep 1
+
+echo ''
+
+echo 'Start iproxy'
+
+./iproxy 2222:44 > /dev/null 2>&1 &
+
+echo "Mounting"
+./sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no -p 2222 "root@localhost" 'mount -o rw,union,update /'
+echo "Mounted!"
+
+echo "patching RaptorActivation.pem"
+./sshpass -p 'alpine' scp -rP 2222 -o StrictHostKeyChecking=no ./RaptorActivation.pem root@localhost:/System/Library/PrivateFrameworks/MobileActivation.framework/Support/Certificates/RaptorActivation.pem
+echo "Done"
+
+
+
 
 # Kill iproxy
-kill %1 > /dev/null 2>&1
+#kill %1 > /dev/null 2>&1
 
-ideviceactivation activate -s https://hackt1vator.github.io/miunlock/activator.php -d
-
-echo 'bypass Done!'
+echo 'Done'
 
 echo ''
